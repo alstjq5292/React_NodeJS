206ed400ec002b0b8c37168ed9ab05db94bfca1a
임민섭 <alstjq5292@naver.com>
2021-07-29 15:15:23 +0900

    리액트 로그인 로그아웃

--- server/index.js
+++ server/index.js
@@ -92,10 +92,10 @@ app.get("/api/users/logout", auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, user) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).send({
-      success: true,
-    });
-  });
-});
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
