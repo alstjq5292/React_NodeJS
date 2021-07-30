0b7e00310c756996f9bf4234f56fd26a9c2341a4
임민섭 <alstjq5292@naver.com>
2021-07-30 16:45:09 +0900

    레이아웃 설정 완료

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,22 +1,18 @@
-import {
-    LOGIN_USER,
-    REGISTER_USER,
-    AUTH_USER
-} from '../_actions/types';
+import { LOGIN_USER, REGISTER_USER, AUTH_USER } from '../_actions/types';
 
-/* eslint-disable import/no-anonymous-default-export */
+
 export default function (state = {}, action) {
     switch (action.type) {
         case LOGIN_USER:
-            return {...state, loginSuccess: action.payload}
+                return {...state, loginSuccess: action.payload};
             break;
 
         case REGISTER_USER:
-            return {...state, register: action.payload}
+                return {...state, register: action.payload}
             break;
 
         case AUTH_USER:
-            return {...state, userData: action.payload}
+                return {...state, userData: action.payload}
             break;    
 
         default:
--- client/src/components/views/HomePage/HomePage.js
+++ client/src/components/views/HomePage/HomePage.js
@@ -1,59 +1,53 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { Layout, Menu, Breadcrumb } from 'antd';
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import "../../../App.css";
+import { Link, Route, withRouter } from 'react-router-dom';
+import LoginPage from "../LoginPage/LoginPage";
 
 function HomePage(props) {
   useEffect(() => {
-    axios.get("/api/hello").then((response) => console.log(response.data));
+    axios.get("/").then((response) => console.log(response.data));
   }, []);
 
   const { SubMenu } = Menu;
   const { Header, Content, Sider } = Layout;
 
+  const onClickHandler = () => {
+    props.history.push('/');
+  }
+
   return (
       <Layout>
         <Header className="header">
           <div className="logo" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-            <Menu.Item key="1">home</Menu.Item>
-            <Menu.Item key="2"></Menu.Item>
-            <Menu.Item key="3">nav 3</Menu.Item>
+          <Menu theme="dark" mode="horizontal" //defaultSelectedKeys={["1"]} // defaultSelectedKeys = 버튼 선택 된 상태 key = 1 이면 1번 버튼 선택 되어 있음
+          >
+            <Menu.Item key="1" onClick={onClickHandler}>Home</Menu.Item>
+            <Menu.Item key="2">About</Menu.Item>
+            <Menu.Item key="3">Project</Menu.Item>
+            <Menu.Item key="4">Contact</Menu.Item>
           </Menu>
         </Header>
         <Layout>
-          <Sider width={200} className="site-layout-background">
+          <Sider width={300} className="site-layout-background">
             <Menu
               mode="inline"
               defaultSelectedKeys={["1"]}
               defaultOpenKeys={["sub1"]}
               style={{ height: "100%", borderRight: 0 }}
             >
-              <SubMenu key="sub1" icon={<UserOutlined />} title="다크웹 크롤링 및 분류">
-                <Menu.Item key="1">option1</Menu.Item>
-                <Menu.Item key="2">option2</Menu.Item>
-                <Menu.Item key="3">option3</Menu.Item>
-                <Menu.Item key="4">option4</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" icon={<LaptopOutlined />} title="아우터">
-                <Menu.Item key="5">option5</Menu.Item>
-                <Menu.Item key="6">option6</Menu.Item>
-                <Menu.Item key="7">option7</Menu.Item>
-                <Menu.Item key="8">option8</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
-                <Menu.Item key="9">option9</Menu.Item>
-                <Menu.Item key="10">option10</Menu.Item>
-                <Menu.Item key="11">option11</Menu.Item>
-                <Menu.Item key="12">option12</Menu.Item>
+              <SubMenu key="sub1" title="Project">
+                <Menu.Item key="1">다크웹 크롤링 및 분류</Menu.Item>
+                <Menu.Item key="2">스피어 피싱 모의훈련 서비스</Menu.Item>
+                <Menu.Item key="3">라즈베리파이를 통한 침입탐지시스템</Menu.Item>
+                <Menu.Item key="4">Toy Project</Menu.Item>
+                <Menu.Item key="5">쇼핑몰 사이트</Menu.Item>
               </SubMenu>
             </Menu>
           </Sider>
           <Layout style={{ padding: "0 24px 24px" }}>
             <Breadcrumb style={{ margin: "16px 0" }}>
-              <Breadcrumb.Item>Home</Breadcrumb.Item>
-              <Breadcrumb.Item>List</Breadcrumb.Item>
-              <Breadcrumb.Item>App</Breadcrumb.Item>
             </Breadcrumb>
             <Content
               className="site-layout-background"
@@ -63,7 +57,6 @@ function HomePage(props) {
                 minHeight: 280,
               }}
             >
-              Content
             </Content>
           </Layout>
         </Layout>
@@ -71,4 +64,4 @@ function HomePage(props) {
   );
 }
 
-export default HomePage;
+export default withRouter(HomePage);
